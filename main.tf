efa724c6800b06d157a7ab80724cb7794744465a
mperetz <mperetz@redhat.com>
2022-12-23 13:05:52 +0200

    04_01 Get ready to scale

--- main.tf
+++ main.tf
@@ -14,25 +14,21 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_vpc" "prod-vpc" {
-    cidr_block = "10.0.0.0/16"
-    enable_dns_support = "true" #gives you an internal domain name
-    enable_dns_hostnames = "true" #gives you an internal host name
-    enable_classiclink = "false"
-    instance_tenancy = "default"    
-    
-    tags = {
-        Name = "prod-vpc"
-    }
-}
 
-resource "aws_subnet" "prod-subnet" {
-  vpc_id            = aws_vpc.prod-vpc.id
-  cidr_block        = "10.0.10.0/24"
-  availability_zone = "us-east-2a"
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-2a","us-east-2b","us-east-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
 
   tags = {
-    Name = "tf-example"
+    Terraform = "true"
+    Environment = "dev"
   }
 }
 
@@ -40,7 +36,7 @@ resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
-  subnet_id 		 = aws_subnet.prod-subnet.id  
+  subnet_id 		 = module.blog_vpc.public_subnets[0]
   
   tags = {
     Name = "blog-tf"
@@ -52,7 +48,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.16.2"
  
-  vpc_id = aws_vpc.prod-vpc.id
+  vpc_id = module.blog_vpc.vpc_id
   name	 = "blog"
 
   ingress_rules 	= ["http-80-tcp", "https-443-tcp"]
